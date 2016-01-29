d07dafe7930789866eb1d8feff6a5fd8e7406ce3
aft-luke <luke.horton@acrospire.technology>
2016-01-29 10:35:06 -0500

    monkey patch eventEmitter fix while waiting for PR

--- src/middleware.js
+++ src/middleware.js
@@ -1,34 +1,56 @@
-import { asap, isDev } from './utils'
+import { asap, noop, check, is, isDev } from './utils'
 import proc from './proc'
 import emitter from './emitter'
 import { MONITOR_ACTION } from './monitorActions'
 
+export const NOT_ITERATOR_ERROR = "runSaga must be called on an iterator"
 
-export default (...sagas) => ({getState, dispatch}) => {
+export default function createSagaMiddleware(...sagas) {
+    const sagaEmitter = emitter()
+    const monitor = isDev ? action => asap(() => dispatch(action)) : undefined
+    let _dispatch
+    function middleware({ getState, dispatch }) {
+        _dispatch = dispatch
 
-  const sagaEmitter = emitter()
-  const monitor = isDev ? action => asap(() => dispatch(action)) : undefined
+          sagas.forEach( saga => {
+            proc(
+              saga(getState),
+              sagaEmitter.subscribe,
+              dispatch,
+              monitor,
+              0,
+              saga.name
+            )
+          })
 
-  sagas.forEach( saga => {
-    proc(
-      saga(getState),
-      sagaEmitter.subscribe,
-      dispatch,
-      monitor,
-      0,
-      saga.name
-    )
-  })
+      return next => action => {
+        const result = next(action) // hit reducers
+        // filter out monitor actions to avoid endless loop
+        // see https://github.com/yelouafi/redux-saga/issues/61
+        if(!action[MONITOR_ACTION])
+          sagaEmitter.emit(action)
+        return result;
+      }
+    }
 
-  return next => action => {
-    const result = next(action) // hit reducers
-    // filter out monitor actions to avoid endless loop
-    // see https://github.com/yelouafi/redux-saga/issues/61
-    if(!action[MONITOR_ACTION])
-      sagaEmitter.emit(action)
-    return result;
-  }
+    middleware.run = (iterator, { subscribe, dispatch }, monitor = noop) => {
+        if (!subscribe) {
+            subscribe = sagaEmitter.subscribe
+        }
 
+        if (!dispatch) {
+            dispatch = _dispatch
+        }
 
+        check(iterator, is.iterator, NOT_ITERATOR_ERROR)
 
+        return proc(
+            iterator
+            , subscribe
+            , dispatch
+            , monitor
+        )
+    }
+
+    return middleware
 }
